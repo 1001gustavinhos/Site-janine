5d9a751407ea5e69e925cc06bf4e998d6ab50dce
Gustavo Alencar <gustavo.asa.dantas@gmail.com>
2025-06-24 21:40:27 -0300

    fix: update metadata titles for consistency in branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,14 @@ const ptSans = PT_Sans({
 
 export const metadata = {
   title: {
-    default: "Psicóloga Janine Correa | Analista do Comportamento - ACT & FAP",
+    default: "Psicóloga Janine Correa",
     template: "%s | Psi. Janine Correa | Analista do Comportamento - ACT & FAP",
   },
   description:
     "Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT).",
   metadataBase: new URL("https://psijaninecorrea.com.br"),
   openGraph: {
-    title: "Psicóloga Janine Correa",
+    title: "Psicóloga Janine Correa | Analista do Comportamento - ACT & FAP",
     description:
       "Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT).",
     url: "/",
