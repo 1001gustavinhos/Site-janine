ace810ce874fff1146e02ce8a324a7af4d637293
Gustavo Alencar <gustavo.asa.dantas@gmail.com>
2025-06-26 22:59:38 -0300

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,30 +26,6 @@ export const metadata: Metadata = {
   description:
     "Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT).",
   metadataBase: new URL("https://psijaninecorrea.com.br"),
-  openGraph: {
-    title: "Psicóloga Janine Correa | Analista do Comportamento - ACT & FAP",
-    description:
-      "Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT).",
-    url: "/",
-    siteName: "Psicóloga Janine Correa",
-    images: [
-      {
-        url: "/images/metaImage.jpeg", // coloque a imagem na pasta public/
-        width: 1200,
-        height: 630,
-        alt: "Imagem de capa do site da Psicóloga Janine Correa",
-      },
-    ],
-    locale: "pt_BR",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Psicóloga Janine Correa",
-    description:
-      "Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT).",
-    images: ["/images/metaImage.jpeg"],
-  },
   icons: {
     icon: "/favicon.ico",
   },
@@ -63,18 +39,42 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${playfair.variable} ${ptSans.variable}`}>
       <head>
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Open Graph / Facebook meta tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/" />
         <meta
-          name="twitter:title"
+          property="og:title"
           content="Psicóloga Janine Correa | Analista do Comportamento - ACT & FAP"
         />
+        <meta
+          property="og:description"
+          content="Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT)."
+        />
+        <meta property="og:image" content="/images/metaImage.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:image:alt"
+          content="Imagem de capa do site da Psicóloga Janine Correa"
+        />
+        <meta property="og:site_name" content="Psicóloga Janine Correa" />
+        <meta property="og:locale" content="pt_BR" />
+
+        {/* Twitter meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Psicóloga Janine Correa" />
         <meta
           name="twitter:description"
           content="Sou Psicóloga Formada pela Unesp em 2018, atuo com o atendimento clinico de adultos desde a conclusão da minha graduação e adoto como abordagem a Terapia Comportamental. Também tenho especialização em Psicoterapia Analítica Funcional (FAP) e em Terapia de aceitação e compromisso (ACT)."
         />
-        <meta name="twitter:image" content="/images/metaImage.jpeg" />
-        <link rel="icon" href="/favicon.ico"></link>
+        <meta name="twitter:image" content="/images/metaImage.jpg" />
+
+        {/* Favicon and canonical URL */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://psijaninecorrea.com.br" />
       </head>
       <body>
         <div id="root">{children}</div>
