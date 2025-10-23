95e21528a76b5583c3d8c0ef24d7abeb42c5699d
Gustavo Alencar <gustavo.asa.dantas@gmail.com>
2025-10-23 01:29:13 -0300

    fix: remove redundant ESLint extension for @next/next plugin

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "plugin:@next/next/recommended",
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -20,6 +16,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@next/next": require("@next/eslint-plugin-next"),
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
